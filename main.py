ee1fb855f1d7a2aeaeb851ad3f3a069732fb94d6
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2024-01-13 11:34:53 -0300

    Creating Menu

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Agile Chef")
+print("Agile Chef \n")
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair \n")
 
 options_choose = input("Escolha um opção: ")
 print(f"A opção escolhida foi: ", options_choose)
