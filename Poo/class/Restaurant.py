e5d7dc9f7aa55a94314ebaf252221d4945b74527
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2024-01-15 17:07:28 -0300

    :up:

--- Poo/class/Restaurant.py
+++ Poo/class/Restaurant.py
@@ -4,7 +4,7 @@ class Restaurant:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.status = False
+        self._status = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
@@ -13,6 +13,10 @@ class Restaurant:
     def list_restaurant(self):
         for restaurant in Restaurant.restaurants:
             print(restaurant)
+
+    @property
+    def active(self):
+        return 'Ativo' if self.active else 'Inativo'
 
 
 sushi = Restaurant('Shushi', 'Comida')
